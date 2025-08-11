15ced34228ca5352cf73a715d89a8fef3787c25e
SHUBHAM VISHWAKARMA <118911100+Shubham6393@users.noreply.github.com>
2025-08-11 06:26:51 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version.
+
+//alert (3+3) // we are using nodeJs, not browser
+console.log(3
+    +
+    3
+)// code redability should be high
+
+console.log("Shubham");
+
+
+let name = "Khushi";
+let age = 22;
+let isGoodLooking = true;
+
+
+// number -> 2 to the power 53
+// bigint
+// string -> ""
+//boolean -> true/false
+// null -> standalone value  - empty value
+// undefined -> declare but not giving any value to it.
+// symbol -> unique
+
+console.log(typeof "Shubham");
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isGoodLooking);
+
+console.log(typeof undefined); // undefined.
+console.log(typeof null);  // object.
