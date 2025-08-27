8d55ef5bb7fb693285fc9a2d4b29def755702195
SHUBHAM VISHWAKARMA <118911100+Shubham6393@users.noreply.github.com>
2025-08-27 10:15:37 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(key);  // if we are applying for-in loop in arr it will print the index of the array.
+    // but if we are using for-of loop then it will directly print value of that index .
+
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);  // not iterate over map().
+// }
