a2b5862bf3b3583341a41739d40a082c388e8142
SHUBHAM VISHWAKARMA <118911100+Shubham6393@users.noreply.github.com>
2025-08-27 09:53:16 +0000

    for - of - loop discussion

---
+++ 05_iterations/three.js
@@ -0,0 +1,41 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")   // it will not occures twice because it consists the unique element.
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+for (const [key, value] of myObject) {   // myObeject - is not iterable here because its an object 
+    //to implement this we have some different syntax. 
+    // console.log(key, ':-', value);
+    
+}
